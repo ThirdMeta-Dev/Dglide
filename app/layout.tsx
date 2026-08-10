e6ec907f5f0a88831bd0bf382f6b15322bf782e4
Hasna Kanchwala <team@hexanovate.in>
2026-08-10 16:11:51 +0530

    feat: add Google Tag Manager sitewide

--- app/layout.tsx
+++ app/layout.tsx
@@ -68,7 +68,26 @@ export default function RootLayout({
       lang="en"
       className={`${sora.variable} ${inter.variable} ${montserrat.variable} h-full antialiased`}
     >
+      <head>
+        <Script id="google-tag-manager" strategy="beforeInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-T23WQK9J');
+          `}
+        </Script>
+      </head>
       <body className="min-h-full flex flex-col bg-[#F3F3F3]">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-T23WQK9J"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         {children}
       </body>
       <Script
