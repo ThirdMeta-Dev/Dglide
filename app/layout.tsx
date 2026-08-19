806fe681aa2a54088a7a96ae6e306c83b72a28de
Hasna Kanchwala <team@hexanovate.in>
2026-08-19 11:00:16 +0530

    Add Microsoft Clarity tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -102,6 +102,15 @@ export default function RootLayout({
           gtag('config', 'G-T5K6SG2E0Z');
         `}
       </Script>
+      <Script id="microsoft-clarity" strategy="afterInteractive">
+        {`
+          (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "xp8x1ifkri");
+        `}
+      </Script>
     </html>
   );
 }
